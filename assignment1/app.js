421183cc4304ffa5b817bfe5c5c598059fbf8f46
Aris Piatti <aris.piatti.bit@tmsgovch.onmicrosoft.com>
2024-04-15 17:26:55 +0200

    Assignment1

---
+++ assignment1/app.js
@@ -0,0 +1,21 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+    $scope.message="";
+     $scope.checkIfTooMuch = function($scope) {
+        let length = $scope.food.split(',').length;
+        if (length === 0) {
+          $scope.message = 'Please enter data first';
+        } else if (length <= 3) {
+          $scope.message = 'Enjoy!';
+        } else {
+          $scope.message = 'Too much!';
+        }
+      };
+
+})();
